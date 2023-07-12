e8b2ae11398f7cf1612ae5b2ce141ecb774dc402
Minh Thanh <86462628+ngthachminhthanh@users.noreply.github.com>
2023-07-12 13:16:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,11 +7,3 @@ document.getElementById('prev').onclick = function() {
     let lists = document.querySelectorAll('.item');
     document.getElementById('slide').prepend(lists[lists.length - 1]);
 }
-
-var buttonInContent = document.querySelectorAll('.button-in-content');
-
-for(var i=0; i<buttonInContent.length; i++){
-    buttonInContent[i].addEventListener('click',function() {
-        alert("Bạn muốn tìm cc gì ở đây ? ");
-    })
-}
