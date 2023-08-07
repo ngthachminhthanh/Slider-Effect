70296f0e192c48474081778af09294212e0cc8b0
Minh Thanh <86462628+ngthachminhthanh@users.noreply.github.com>
2023-08-07 16:05:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+const videoTest = document.getElementById('video-test');
+// console.log(videoTest.offsetWidth+'px');
+// console.log(videoTest.offsetHeight+'px');
+
+const containerTest = document.querySelector('.container-test');
+// console.log(containerTest.offsetWidth+'px');
+// console.log(containerTest.offsetHeight+'px');
+
+// logic to do: 
+// containerTest.offsetWidth = videoTest.offsetWidth;
+// containerTest.offsetHeight = videoTest.offsetHeight;
+
+// set the real width of video displayed to width of container
+var r = document.querySelector(':root');
+var rs = getComputedStyle(r);
+r.style.setProperty('--container-width', videoTest.offsetWidth+'px');
+r.style.setProperty('--container-height', videoTest.offsetHeight+'px');
+
 document.getElementById('next').onclick = function() {
     let lists = document.querySelectorAll('.item');
     document.getElementById('slide').appendChild(lists[0]);
